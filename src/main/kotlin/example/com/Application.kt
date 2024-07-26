0d47fd37ea8c9705a20d52a56a95be457acdca42
Wesley Maciel <wesley.2510@hotmail.com>
2024-07-26 14:47:30 -0300

    Security Plugin Working as Expected, tested in Postman

--- src/main/kotlin/example/com/Application.kt
+++ src/main/kotlin/example/com/Application.kt
@@ -5,8 +5,10 @@ import io.ktor.server.application.*
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
+    //embeddedServer(Netty, port = 8080, module = Application::module).start(wait = true)
 }
 
 fun Application.module() {
+    configureSecurity()
     configureRouting()
 }
--- src/main/kotlin/example/com/plugins/Routing.kt
+++ src/main/kotlin/example/com/plugins/Routing.kt
@@ -3,15 +3,11 @@ package example.com.plugins
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import io.ktor.server.netty.*
-import io.ktor.server.engine.*
 
 fun Application.configureRouting() {
-    embeddedServer(Netty, 8080) {
-        routing {
-            get("/") {
-                call.respondText("Hello World!")
-            }
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
         }
-    }.start(wait = true)
+    }
 }
